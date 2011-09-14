7dd8b6e0babc6e605bb14f7cfc04c8f2254d7a8a
Eric Yim <nacchii@gmail.com>
2011-09-14 03:53:47 -0700

    Initial Commit

---
+++ NDControlSliderExample/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  NDControlSliderExample
+//
+//  Created by Eric Yim on 11-09-14.
+//  Copyright N/A 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ NDControlSliderExample/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NDControlSliderExample
+//
+//  Created by Eric Yim on 11-09-14.
+//  Copyright N/A 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
